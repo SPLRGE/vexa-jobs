fd53d1538997fa6c30ab9c75cf7979d5ad813a32
Julien <61233505+dev-cetus@users.noreply.github.com>
2023-11-13 11:18:03 +0100

    refactor: remove rule "Delete 'CR'"

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,7 @@ module.exports = {
   },
   extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
   plugins: [],
-  rules: {},
+  rules: {
+    "prettier/prettier": ["error", { "endOfLine": "auto" }]
+  },
 }
